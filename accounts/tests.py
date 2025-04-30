1cad58a11503e07395d6492606c1e5b7c11a93c7
AshkanPourabdollah <ashkanp09@gmail.com>
2025-04-30 16:02:09 +0330

    crispy forms bootstrap5 added

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+class SignupTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_signup_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_url_by_url(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_form(self):
+        user = get_user_model().objects.create_user(
+           'usernametest',
+           'usernametest@gmail.com',
+        )
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, 'usernametest')
+        self.assertEqual(get_user_model().objects.all()[0].email, 'usernametest@gmail.com')
+
+
