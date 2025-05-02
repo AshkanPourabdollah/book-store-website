af6c023b46db59ce0ee038cf8edef1cf3ccbd5bf
AshkanPourabdollah <ashkanp09@gmail.com>
2025-05-02 12:34:41 +0330

    Static files with front end files added

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -135,3 +134,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Email Config
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Static file config
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
