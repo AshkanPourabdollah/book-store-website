bf2eb08c7b9330979963e4bb461920b6bf786d7b
AshkanPourabdollah <ashkanp09@gmail.com>
2025-05-04 22:47:47 +0330

    book comments and adding new comment added

--- books/views.py
+++ books/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.urls import reverse_lazy
 from .models import Book
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -16,6 +17,27 @@ class BookDetailView(generic.DetailView):
     model = Book
     template_name = "books/book_detail.html"
     context_object_name = 'book'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.all()
+        context['form'] = CommentForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.book = self.object
+            comment.user = request.user
+            comment.save()
+            return redirect('book_detail', pk=self.object.pk)
+
+        context = self.get_context_data()
+        context['form'] = form
+        return self.render_to_response(context)
 
 
 class BookCreateView(generic.CreateView):
